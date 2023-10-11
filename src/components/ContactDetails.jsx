9cbadc8fc8218a7c0e0ad5d620dab8fcf1e07c75
Nirjhar sarkar <nirjharsarkar8@gmail.com>
2023-10-11 19:41:46 +0530

    contact done

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -1,35 +1,68 @@
 "use client";
 
 
+import { useRef, useState } from "react";
+import Contact from "./ContactForm";
 import TextInput from "./TextInput";
+import emailjs from "@emailjs/browser";
 
 
 
 export default function ContactForm() {
-     
-      return (
-            <form>
-                  <h2 className="font-display text-base font-semibold text-neutral-950">
-                        Work inquiries
-                  </h2>
-                  <div className="isolate mt-6 -space-y-px rounded-2xl bg-white/50">
-                        <TextInput label="Name" name="name" autoComplete="name" />
-                        <TextInput
-                              label="Email"
-                              type="email"
-                              name="email"
-                              autoComplete="email"
-                        />
+      const [error, setError] = useState(false);
+      const [success, setSuccess] = useState(false);
+      const formRef = useRef();
 
-                        {/* <TextInput label="Phone" type="tel" name="phone" autoComplete="tel" /> */}
-                        <TextInput label="Message" name="message" />
-                  </div>
-                  <button
-                        type="submit"
-                        // disabled={loading}
-                        className="px-4 py-2 w-40 bg-gray-700 disabled:bg-gray-400 disabled:text-gray-100 text-white font-medium mt-4">
-                        Send Message
-                  </button>
-            </form >
+      const sendEmail = (e) => {
+            e.preventDefault();
+
+            emailjs
+                  .sendForm(
+                        "service_p8n2mk7",
+                        "template_j43h9vg",
+                        formRef.current,
+                        "pOWs27XlCv1P2IyJ0"
+                  )
+                  .then(
+                        (result) => {
+                              setSuccess(true)
+                        },
+                        (error) => {
+                              setError(true);
+                        }
+                  );
+      };
+
+
+      return (
+            <>
+                  <form
+                        ref={formRef}
+                        onSubmit={sendEmail}
+                  >
+                        <h2 className="font-display text-base font-semibold text-neutral-950">
+                              Work inquiries
+                        </h2>
+                        <div className="isolate mt-6 -space-y-px rounded-2xl bg-white/50">
+                              <TextInput type="text" label="Name" required placeholder="Name" name="name" autoComplete="name" />
+                              <TextInput
+                                    type="email" required placeholder="Email" name="email"
+                                    label="Email"
+                                    autoComplete="email"
+                              />
+
+                              {/* <TextInput label="Phone" type="tel" name="phone" autoComplete="tel" /> */}
+                              <TextInput placeholder="Message" name="message" label="Message" />
+                        </div>
+                        <button
+                              type="submit"
+                              // disabled={loading}
+                              className="px-4 py-2 w-52 rounded-lg bg-gray-700 disabled:bg-gray-400 disabled:text-gray-100 text-white font-medium mt-4">
+                              Send Message{' '}
+                              {error && "Error"}
+                              {success && "Success"}
+                        </button>
+                  </form >
+            </>
       );
 }
